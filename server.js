a99a72ce43bd062d3cde84aa0a1ea65fe033258f
Daniel Eckton <dseckton@gmail.com>
2022-04-28 10:52:49 -0600

    added mongodb

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
+const contactsRoutes = require('./routes/contacts');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
-app.use('/', require('./routes'));
+app.use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/contacts', contactsRoutes);
 
-app.listen(port, () => {
-  console.log(`Running on port ${port}`)
-})
+  mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to DB and listening on port ${port}`)
+    }
+  })
