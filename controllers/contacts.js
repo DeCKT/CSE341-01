81b2f837bd6be715cdd9e0a426c379eb939cb259
Daniel Eckton <dseckton@gmail.com>
2022-05-25 09:09:30 -0600

    my life is miserable

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -14,7 +14,7 @@ const getById = async (req, res, next) => {
     try {
         const userId = new ObjectId(req.params.id);
         const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
-        result.toArray(err, lists).then((lists) => {
+        result.toArray().then((lists) => {
             res.setHeader('Content-Type', 'application/json');
             res.status(200).json(lists[0]);
             next();
